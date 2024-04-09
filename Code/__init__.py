48fc02b9e712fa5f28ea2eff64f35b49ecb07d73
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-04-09 15:07:22 +0200

    deleted a bunch of useless stuff

--- Code/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import readext
-from . import movef
--- Code/main.py
+++
@@ -1,9 +0,0 @@
-import Code
-
-rf = Code.readext.readfolder()
-mf = Code.movef.change()
-
-if __name__ == "__main__":
-
-    mf.checkdefs()
-    rf.prepfiles()
