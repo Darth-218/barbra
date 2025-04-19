180b51f694bec1d0a276492ddbd6d35f18b7575b
darth-218 <yahia.hany.gaber@proton.me>
2025-04-20 00:25:39 +0200

    Finished the MVP

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from pathlib import Path
+from yaml import safe_load
+from shutil import move
 
 def getContent(path: Path) -> tuple[list[str], list[str]]:
     entries = ([], [])
     for entry in path.iterdir():
-        dest = 0 if entry.is_dir() else 1
+        dest = 1 if entry.is_dir() else 0
         entries[dest].append(entry)
     return entries
 
 
 def moveFiles(entries: tuple, dest: dict) -> None:
-    entries = entries
-    dest = dest
-    pass
+    file: Path
+    for file in entries[0]:
+        try:
+            move(file, dest[file.as_posix().split(".")[-1]])
+        except KeyError:
+            move(file, dest['other'])
+    for directory in entries[1]:
+        move(directory, dest['directory'])
 
 
 def readConfig(config: Path) -> dict:
-    config = config
-    return {}
+    with open(config, 'r') as config_file:
+        return safe_load(config_file)
 
 
-print(getContent(Path('/home/darth/Downloads/')))
+if __name__ == '__main__':
+    config_path = Path('./config.yaml')
+    config = readConfig(config_path)
+    content = getContent(Path(config['source']))
+    moveFiles(content, config)
+    print("Files moved!")
